436fb52401c1c6ab4a2a9799e95233883d51ea87
Roman Zaitsev <66490718+Tefix@users.noreply.github.com>
2025-01-28 15:54:43 +0200

    Update opilane.sql

--- opilane.sql
+++ opilane.sql
@@ -44,3 +44,65 @@ select * from opilane;
 
 UPDATE opilane SET aadress=´Tallinn´
 WHERE opilaneId= 3;
+
+CREATE TABLE language
+(
+ID int not null primary key,
+Code char(3) NOT NULL,
+Language varchar(50) NOT NULL,
+IsOfficial bit,
+Percentage smallint
+);
+select * from Language
+
+INSERT INTO Language(ID, code, Language)
+VALUES (2, 'rus', 'vene'), (3, 'eng','inglise'),(4, 'DE', 'saksa')
+Create table keeleValik
+(
+keelevalikId int primary key identity (1,1),
+valikuNimetus varchar(10) not null,
+opilaneId int, 
+Foreign key (opilaneId) references opilane(opilaneId),
+Language int,
+Foreign key (language) references Language(ID)
+)
+select * from keelevalik;
+select * from Language;
+select * from opilane;
+
+Insert into keeleValik(valikuNimetus,opilaneId, Language)
+Values ('valik d', 5, 2)
+
+delete from keeleValik where keeleValikI=6;
+
+select opilane.eesnimi, Language.Language
+from opilane, language, keelevalik
+where opilane.opilaneId=keelevalik.opilaneID
+and Language.ID=keelevalik.Language
+
+select * 
+from opilane, language, keelevalik
+where opilane.opilaneId=keelevalik.opilaneID
+and Language.ID=keelevalik.Language
+
+create table oppimine1
+(
+oppimineId int primary key identity(1,1),
+aine varchar(10),
+aasta date,
+opetaja varchar(20),
+opilaneId int,
+Foreign key (oppimineId) references opilane(opilaneId),
+hinne int)
+
+INSERT INTO oppimine (aine, aasta, opetaja, opilaneId, hinne)
+VALUES ('eesti', '2025-01-01', 'Oleinik',1, 4);
+
+select * from keelevalik;
+select * from Language;
+select * from opilane;
+select * from oppimine1
+
+
+---------------------------------------
+vigane kood
